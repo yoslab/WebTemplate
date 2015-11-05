a1791f1ae36d7124c39fdcdd048659ea59421661
Layu <enlosph@gmail.com>
2015-11-06 01:16:40 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="robots" content="noindex, nofollow">
+<!--[if lt IE 9]>
+<script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<title>ページタイトル</title>
+<meta name="description" content="ページの説明">
+<meta name="author" content="Sakusha">
+<!-- ファビコン -->
+<link rel="shortcut icon" href="">
+
+<!-- Normalize -->
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
+<!-- Bootstrap -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!-- Smoke _ Bootstrapの拡張的プラグイン -->
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/smokejs/2.2.4/css/smoke.min.css">
+<!-- Custom CSS -->
+<link rel="stylesheet" href="./css/style.css">
+
+</head>
+<body>
+
+<div id="main">
+    コンテンツ
+</div>
+
+<!-- Jquery, Bootstrap -->
+<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+<!-- <script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script> -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<!-- Smoke _ Bootstrapの拡張的プラグイン -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/smokejs/2.2.4/js/smoke.min.js"></script>
+
+<!-- Custom JS -->
+<script src="./js/main.js"></script>
+
+</body>
+</html>
