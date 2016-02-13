e819c5915afa6fbec2476e7e010fe93e3f406bec
Layu <enlosph@gmail.com>
2016-02-14 03:00:21 +0900

    modify bootstrap v4

--- simple/index.php
+++ simple/index.php
@@ -1,49 +1,46 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="robots" content="noindex, nofollow">
-<!--[if lt IE 9]>
-<script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-<title>ページタイトル</title>
-<meta name="description" content="ページの説明">
-<meta name="author" content="Sakusha">
-<!-- ファビコン -->
-<link rel="shortcut icon" href="">
-
-<!-- Normalize -->
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
-<!-- Bootstrap -->
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<!-- 任意の拡張的プラグイン -->
-<!-- <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/smokejs/2.2.4/css/smoke.min.css"> -->
-<!-- Custom CSS -->
-<link rel="stylesheet" href="./css/style.css">
-
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="noindex, nofollow">
+  <!--[if lt IE 9]>
+    <script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <title>ページタイトル</title>
+  <meta name="description" content="ページの説明">
+  <meta name="author" content="Sakusha">
+  <!-- ファビコン -->
+  <link rel="shortcut icon" href="">
+  <!-- Normalize -->
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
+  <!-- Bootstrap v4: http://getbootstrap.com/css/ -->
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css">
+  <!-- Font Awesome: https://fortawesome.github.io/Font-Awesome/icons/ -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- 任意の拡張的プラグイン -->
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
 
-<!-- ここからレイアウト -->
-
-<div id="main">
+  <!-- ************ ここからレイアウト ************ -->
+  <div id="main">
     コンテンツ
-</div>
+  </div>
+  <!-- ************ ここまでレイアウト ************ -->
 
-<!-- ここまでレイアウト -->
+  <!-- Jquery, Bootstrap -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <!-- <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script> -->
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js"
+    integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7"
+    crossorigin="anonymous"></script>
+  <!-- 任意の拡張的プラグイン -->
 
-<!-- Jquery, Bootstrap -->
-<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-<!-- <script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script> -->
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<!-- 任意の拡張的プラグイン -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/smokejs/2.2.4/js/smoke.min.js"></script>
-<!-- Custom JS -->
-<script src="./js/main.js"></script>
-
+  <!-- Custom JS -->
+  <script src="./js/main.js"></script>
 </body>
 </html>
