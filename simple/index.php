ca7d2882662c0374ee2f6b30b4c516bace2accca
Layu <enlosph@gmail.com>
2016-02-15 19:47:07 +0900

    fix shrink-to-fitに対応，normalizeをmin化ほか

--- simple/index.php
+++ simple/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="robots" content="noindex, nofollow">
   <!--[if lt IE 9]>
     <script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -15,9 +15,9 @@
   <!-- ファビコン -->
   <link rel="shortcut icon" href="">
   <!-- Normalize -->
-  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css">
   <!-- 任意の拡張的プラグイン -->
-  
+  <!-- <link rel="stylesheet" href=""> -->
   <!-- Custom CSS -->
   <link rel="stylesheet" href="./css/style.css">
 </head>
@@ -30,7 +30,7 @@
   <!-- ************ ここまでレイアウト ************ -->
 
   <!-- 任意の拡張的プラグイン -->
-
+  <!-- <script src=""></script> -->
   <!-- Custom JS -->
   <script src="./js/main.js"></script>
 </body>
