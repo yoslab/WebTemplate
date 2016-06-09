7e9b082ba9000a9d8a91e5f968e79dc6360f5c99
Layu <enlosph@gmail.com>
2016-06-09 13:19:18 +0900

    modify StarterTemplateの利用

--- bootstrap-basic/index.php
+++ bootstrap-basic/index.php
@@ -26,6 +26,21 @@
 <body>
 
   <!-- ************ ここからレイアウト ************ -->
+
+  <nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
+    <a class="navbar-brand" href="#">Project name</a>
+    <ul class="nav navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </nav>
   <div class="container">
     <div class="page-header">
       <h1>Bootstrap Basic Examples</h1>
