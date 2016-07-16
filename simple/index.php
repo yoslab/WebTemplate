e5a79c378bf7024ecb4cc92a0bcdb23cdf05e269
Layu <enlosph@gmail.com>
2016-07-16 21:48:04 +0900

    modify cdnjsに修正

--- simple/index.php
+++ simple/index.php
@@ -5,19 +5,17 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
   <meta name="robots" content="noindex, nofollow">
-  <!--[if lt IE 9]>
-    <script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
-  <![endif]-->
   <title>ページタイトル</title>
   <meta name="description" content="ページの説明">
   <meta name="author" content="Sakusha">
   <!-- ファビコン -->
   <link rel="shortcut icon" href="">
+  <!--[if lt IE 9]>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
   <!-- Normalize -->
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css">
-  <!-- 任意の拡張的プラグイン -->
-  <!-- <link rel="stylesheet" href=""> -->
   <!-- Custom CSS -->
   <link rel="stylesheet" href="./css/style.css">
 </head>
@@ -29,8 +27,6 @@
   </div>
   <!-- ************ ここまでレイアウト ************ -->
 
-  <!-- 任意の拡張的プラグイン -->
-  <!-- <script src=""></script> -->
   <!-- Custom JS -->
   <script src="./js/main.js"></script>
 </body>
